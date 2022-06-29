8e5f29e00b2d6a339d82e3bf159ce82bc02e4974
Wan Muzaffar <wanmuz86@gmail.com>
2022-06-29 11:28:10 +0800

    add item

--- lib/widgets/add.dart
+++ lib/widgets/add.dart
@@ -5,9 +5,41 @@ class AddPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var nameEditingController = TextEditingController();
+    var descEditingController = TextEditingController();
+    var placeEditingController = TextEditingController();
     return Scaffold(
-        appBar: AppBar(title:Text("Add Pahe")),
-        body:Text("Add page")
-    );
+        appBar: AppBar(title: Text("Add Page")),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(hintText: "Enter item name"),
+                controller: nameEditingController,
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: "Enter item description"),
+                controller: descEditingController,
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: "Enter item place"),
+                controller: placeEditingController,
+              ),
+              TextButton(
+                  onPressed: () {
+                    var newItem = {
+                      "name": nameEditingController.text,
+                      "desc": descEditingController.text,
+                      "place": placeEditingController.text
+                    };
+                    print(newItem);
+                    // Close this page and pass the newItem forward
+                    Navigator.pop(context,newItem);
+                  },
+                  child: Text("Add new Item"))
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/detail.dart
+++ lib/widgets/detail.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({Key? key}) : super(key: key);
+  // 1) Create a variable that will store the item passed from first page
+  final todoItem;
+  //2) Create a constructor that will receive the variable
+  DetailPage({required this.todoItem});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title:Text("Detail")),
-        body:Text("Detail page")
+        body:Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                Text(todoItem["name"], style: TextStyle(fontSize: 32, color: Colors.red),),
+                SizedBox(height: 8,),
+                Text(todoItem["place"], style: TextStyle(fontSize: 20, color: Colors.blue)),
+                SizedBox(height: 8,),
+                Text(todoItem["desc"],style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
